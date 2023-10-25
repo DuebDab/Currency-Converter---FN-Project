bb6e78721bb4d38c80f52d8903cae3202aaccdea
Sirintip Yosakorn <y.sirintip@hotmail.com>
2023-10-25 19:19:01 +0700

    forex request

--- Currency Converter.py
+++ Currency Converter.py
@@ -1,11 +1,18 @@
 import requests
 import tkinter as tk
-import json
+import urllib, json
 
     
 url = "https://openexchangerates.org/api/latest.json?app_id=cfba8a45cda24635a6b429a67e0a0607"
 data = [i for i in requests.get(url).json()['rates']]
 data.sort()
+
+response = urllib.request.urlopen(url)
+full = json.loads(response.read())
+
+calc = full["rates"]
+
+print(calc)
 
 #UI
 root = tk.Tk()
