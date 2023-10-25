c930ecd7608cb2958a4430a87aee3f0349c9755f
Sirintip Yosakorn <y.sirintip@hotmail.com>
2023-10-25 22:03:31 +0700

    Added some UIs and created calculation class

--- Currency Converter.py
+++ Currency Converter.py
@@ -1,16 +1,28 @@
 import requests
 import tkinter as tk
+from ttkwidgets.autocomplete import AutocompleteCombobox
 import urllib, json
+import decimal
 
 #Extraction
 url = "https://openexchangerates.org/api/latest.json?app_id=cfba8a45cda24635a6b429a67e0a0607"
-data = [i for i in requests.get(url).json()['rates']]
-data.sort()
-
 response = urllib.request.urlopen(url)
 full = json.loads(response.read())
-calc = full["rates"] #Dictionary of all currency exchange USD based
+rates = full["rates"] #Dictionary of all currency exchange USD based
+currencylist = [i for i in rates]
 
+class Calculate:
+    def __innit__(self,base,converted):
+        self.base = decimal.Decimal(base)
+        self.converted = decimal.Decimal(converted)
+    def toUSD(self, base):
+        self.base = decimal.Decimal(base)
+        self.converted = round((self.base / decimal.Decimal(rates.get(self))),4)
+        return self.converted
+    def toOTHER(self, base):
+        self.base = decimal.Decimal(base)
+        self.converted = round((self.base * decimal.Decimal(rates.get(self))),4)
+        return self.converted
 
 #UI
 root = tk.Tk()
@@ -18,10 +30,23 @@ root.title("Currency Converter")
 root.geometry("500x400")
 root.resizable(height = False, width = False)
 #widgets 
-inn = tk.Entry(root, width = 30)
+labelInput = tk.Label(root, text = "From:")
+inputbox = tk.Entry(root, width = 30)
+labelOutput = tk.Label(root, text = "To:")
+labelOutput2 = tk.Label(root, textvariable= "")
+
 
 currency = tk.StringVar()
 currency.set("USD")
 
-dropdown = tk.OptionMenu(root, currency, *data).pack()
+dropdown = AutocompleteCombobox(width=10, completevalues=currencylist)
+dropdown2 = AutocompleteCombobox(width=10, completevalues=currencylist)
+
+#Placement
+labelInput.grid(row=0, column=0, sticky=tk.E, padx=5, pady=5)
+inputbox.grid(row=0, column=1, sticky=tk.W, padx=5, pady=5)
+dropdown.grid(row=0, column=2, sticky=tk.E, padx=5, pady=5)
+labelOutput.grid(row=1, column=0, sticky=tk.E, padx=5, pady=5)
+labelOutput2.grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
+
 root.mainloop()
