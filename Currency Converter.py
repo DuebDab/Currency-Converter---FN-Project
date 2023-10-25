cca381dd0f436245d309c2ab4158a8d3c50e0ff8
Sirintip Yosakorn <y.sirintip@hotmail.com>
2023-10-25 19:20:16 +0700

    dict added

--- Currency Converter.py
+++ Currency Converter.py
@@ -2,17 +2,15 @@ import requests
 import tkinter as tk
 import urllib, json
 
-    
+#Extraction
 url = "https://openexchangerates.org/api/latest.json?app_id=cfba8a45cda24635a6b429a67e0a0607"
 data = [i for i in requests.get(url).json()['rates']]
 data.sort()
 
 response = urllib.request.urlopen(url)
 full = json.loads(response.read())
+calc = full["rates"] #Dictionary of all currency exchange USD based
 
-calc = full["rates"]
-
-print(calc)
 
 #UI
 root = tk.Tk()
